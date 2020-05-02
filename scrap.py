dfd17813b2c6bd98110f81d0f0e55bb9fa9a5621
RenanDias12 <52115774+RenanDias12@users.noreply.github.com>
2020-05-02 13:12:35 -0300

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,6 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = 'https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
+page = requests.get(url) 
+soup = BeautifulSoup(page.text, 'html.parser') 
+#acessando a url e extraindo o site em um BS object
+
+document = open('SiteHTML.txt', 'w') #vai sobrescrever o arquivo anterior
+document.write(soup.prettify())
+
+
+
+
+
+
